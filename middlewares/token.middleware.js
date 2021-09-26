836bb1237c49e20b2af5f0409c1ce448e3c75b12
merkstack <merkstack@merkstack.dev>
2021-09-26 12:16:49 +0000

    error-codes -optional

--- middlewares/token.middleware.js
+++ middlewares/token.middleware.js
@@ -5,10 +5,10 @@ exports.authenticateToken = (req, res, next) => {
 
 	const token = req.headers['auth-token'];
 
-	if (!token) return res.status(401).send({ message: "Data Access Denied!" });
+	if (!token) return res.status(401).send({ code: '01', message: "Data Access Denied!" });
 
 	jwt.verify(token, process.env.APP_TOKEN_SECRET, function (err, decoded) {
-		if (err) return res.status(400).send({ message: "Invalid Token!" });
+		if (err) return res.status(400).send({ code: '02', message: "Invalid Token!" });
 
 		req.user_id = decoded._id;
 		next();
