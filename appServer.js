8bfe7cdb6df2bf841e1af449b2ddf45b859186b4
merkstack <merkstack@merkstack.dev>
2021-09-27 03:25:51 +0000

    cors update

--- appServer.js
+++ appServer.js
@@ -15,12 +15,23 @@ if (dotenv.error) {
 
 const port = process.env.PORT;
 
-// middlewares
-app.use(cors({
-	origin: 'https://wish.aladdinb2b.com',
-	optionsSuccessStatus: 200
-}));
+// cors options
+const whitelist = ['https://wish.aladdinb2b.com']
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (whitelist.indexOf(origin) !== -1) {
+			callback(null, true)
+		} else {
+			callback(new Error('Not allowed by CORS'))
+		}
+	},
+	methods: ['GET', 'POST'],
+	credentials: true
+}
 
+
+// middlewares
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // file hosting for app : disabled
